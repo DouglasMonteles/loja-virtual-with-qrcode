bc3febf67941dda87f929cd69a4f65572f80e8a6
douglasmonteles <silvadouglas328@gmail.com>
2022-10-01 20:16:50 -0300

    Add side nav

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,15 +7,35 @@ import { AppComponent } from './app.component';
 
 import { CartComponent } from './pages/cart/cart.component';
 import { ProductsComponent } from './pages/products/products.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     CartComponent,
     ProductsComponent,
+    SideNavComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
   providers: [
 
   ],
